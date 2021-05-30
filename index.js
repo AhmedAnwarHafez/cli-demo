54bb8c26072cbc97d1ab645a8527c83075bcb002
ahmad-anwar <eng.ahmed.anwar@gmail.com>
2021-05-30 13:29:45 +1200

    use readFile and writeFile in interactive CLI

--- index.js
+++ index.js
@@ -2,26 +2,69 @@ const prompt = require('prompt')
 const fs = require('fs')
 const util = require('util')
 const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+const question = util.promisify(prompt.get)
 
-const main = () => {
-    console.log('A CLI program that prints the contents of any text file');
+main = () => {
+    console.clear();
+    console.log('-------------------------------------------------------');
+    console.log('| A CLI program that prints the contents of any text file |');
+    console.log('-------------------------------------------------------');
     prompt.start()
 
-    const properties = [
-        {
-            name: 'filePath',
-            description: 'Enter the path of the file you want to view (relative or absolute)',
-        }
-    ];
+    const run = async () => {
+        const properties = [
+            {
+                name: 'cmd',
+                description: 'Enter your command',
+            }
+        ];
 
-    prompt.get(properties, async (error, input) => {
-        
-        if(error) {
+        try {
+            viewOptions()
+            const input = await question(properties)
+
+            switch (input.cmd) {
+                case 'q':
+                    process.exit()
+                    break;
+                case 'v': {
+
+                    const input = await question([{ name: 'filePath' }])
+                    const contents = await readFile(input.filePath, 'utf-8')
+                    console.log(contents);
+                    run()
+                    break;
+                }
+                case 'w': {
+
+                    const input = await question([{ name: 'filePath' }, { name: 'contents', description: 'Write the contents' }])
+                    await writeFile(input.filePath, input.contents)
+                    console.log('Operation completed!');
+                    run()
+                    break;
+                }
+                default:
+                    run()
+                    break;
+            }
+        } catch (error) {
+
             console.log(error);
         }
-        const contents = await readFile(input.filePath, 'utf-8')
-        console.log(contents);
-    })
+    }
+
+    run()
+}
+
+
+const viewOptions = () => {
+    const cmdOptions = `
+'v' to view a file
+'w' to write to a file
+'q' to quit`
+    console.log(cmdOptions);
+    console.log('\n');
 }
 
 main()
